13b81511bc584a5d96dac3f513f8eeb0a89cf678
Jason Walton <jwalton@solinkcorp.com>
2021-03-20 11:31:49 -0400

    build: Makefile.

--- generate.sh
+++
@@ -1,4 +0,0 @@
-go generate ./...
-cp ./internal/generator/gchalkgen/gchalkgen.go.txt ./internal/generator/gchalkgen/gchalkgen.go
-go run ./internal/generator/gchalkgen/gchalkgen.go > generated.go
-gofmt -s -w generated.go
