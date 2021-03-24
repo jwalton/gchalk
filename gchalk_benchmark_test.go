12e5f7173a21e6a90f58b9a1b11b94ae6d70ae91
Jason Walton <jwalton@solinkcorp.com>
2021-03-24 08:53:34 -0400

    test: Minor changes to performance benchmarks.

--- gchalk_benchmark_test.go
+++ gchalk_benchmark_test.go
@@ -7,38 +7,52 @@ import (
 var benchGChalk = New(ForceLevel(LevelAnsi16m))
 
 func BenchmarkColor(b *testing.B) {
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		benchGChalk.Red("Hello world!")
 	}
 }
 
+var red = benchGChalk.Red
+
+func BenchmarkPreDefinedColor(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		red("Hello world!")
+	}
+}
+
 func BenchmarkRgb(b *testing.B) {
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		benchGChalk.RGB(10, 30, 255)("Hello world!")
 	}
 }
 
-func BenchmarkColorNoSetup(b *testing.B) {
-	benchGChalk.Red("Hello world!")
-	b.ResetTimer()
+var rgbTest = benchGChalk.RGB(10, 30, 255)
+
+func BenchmarkPreDefinedRgb(b *testing.B) {
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		benchGChalk.Red("Hello world!")
+		rgbTest("Hello world!")
 	}
 }
-
 func BenchmarkComposed(b *testing.B) {
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		benchGChalk.WithBgBlue().WithBold().Red("Hello world!")
 	}
 }
 
 func BenchmarkNested(b *testing.B) {
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		benchGChalk.Red("Hello", benchGChalk.Green("green"), "world!")
 	}
 }
 
 func BenchmarkStyle(b *testing.B) {
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		benchGChalk.StyleMust("red")("Hello world!")
 	}
